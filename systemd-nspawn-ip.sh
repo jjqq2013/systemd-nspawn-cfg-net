ff3f07d5e663f2888668a83ab1a870f440016671
jjqq <5234060+jjqq2013@users.noreply.github.com>
2020-01-14 17:02:19 +0900

    Update systemd-nspawn-ip.sh

--- systemd-nspawn-ip.sh
+++ systemd-nspawn-ip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PS4='+# ' # set trace output prefix char to differ from normal trace prefix('+ ')
 set -e -o pipefail -x # exit on error, enable command trace
 
 # this script configure ip and nat for a systemd-nspawn container and print its ip.
@@ -12,7 +13,7 @@ set -e -o pipefail -x # exit on error, enable command trace
 # note: do not use `export VAR=$(command)` because that will not exit on error
 
 # 10.x.x.x is a private ip range. We will use this range to manage 64(256/4) containers
-IP_PREFIX=10.8.8
+[[ $IP_PREFIX ]] || IP_PREFIX=10.8.8 # set default IP_PREFIX
 
 if [[ $FLOCK_DONE != 1 ]]; then
   CTR_NAME=${1:?require next argument as a running container name, usually the name of the image file or dir}
@@ -21,7 +22,7 @@ if [[ $FLOCK_DONE != 1 ]]; then
   # Note: machinectl show ... --value option is not supported in Xenial.
   CTR_PID=$(machinectl show "$CTR_NAME" --property=Leader | grep -E --only-matching '[0-9]+')
   # systemd-nspawn will use only the first 11 chars to build veth name
-  HOST_VETH_NAME=ve-$(cut --characters=1-11 <<< "$CTR_NAME")
+  HOST_VETH_NAME=ve-${CTR_NAME:0:11}
 
   ################################################################################
   # run myself within an exclusive lock
@@ -34,71 +35,48 @@ __EOF_FLOCK_STDIN
   # will never not come here. The exec command will replace current process
 fi
 
-# get all ip related the ip range, both host and containers, encode them into a comma started and comma separated string,
-# e.g. ",10.8.8.1,172.1.1.1,"
-IP_LIST=,$((machinectl list --max-addresses=all | grep --perl-regexp --only-matching "(?<= )$IP_PREFIX\.[0-9]+(?=(?:,|$))" | sort --unique || true) | tr '\n' ',')
-
-function config_ip() {
-  if [[ $_VETH_NAME == host0 ]]; then
-    j=2 # ip inner index for for container side
-  else
-    j=1 # ip inner index for for host side
-  fi
-
-  if _IP=$(ip -4 -oneline address show dev $_VETH_NAME scope global | grep --perl-regexp --only-matching "(?<=inet )$IP_PREFIX\.[0-9]+/[0-9]+(?= )"); then
-    _IP=${_IP%/*}
-  else
-    # if it has not been assigned an ip with specified range, then assign an unused ip
-    FOUND=0
-    for i in {0..63}; do
-      _IP=$IP_PREFIX.$((i*4+j))
-      # found if the ip is not included in the ip list
-      if [[ $IP_LIST != *,$_IP,* ]]; then
-        FOUND=1
-        break
-      fi
-    done
-    if [[ $FOUND != 1 ]]; then
-      echo "no free ip in $IP_PREFIX.*/30" >&2
-      exit 1
-    fi
-    # it is very important that each veth pair use different broadcast ip, that is why here use /30
-    ip address add $_IP/30 broadcast + dev $_VETH_NAME
-  fi
-
-  ip link set $_VETH_NAME up
-}
+ip link set $HOST_VETH_NAME up
+nsenter --target=$CTR_PID --net ip link set host0 up
 
 ################################################################################
 # assign host side veth ip if not yet
-_VETH_NAME=$HOST_VETH_NAME
-config_ip
-HOST_IP=$_IP
-
-if [[ $IP_LIST != *,$HOST_IP,* ]]; then
-  IP_LIST=$IP_LIST,$HOST_IP,
+if HOST_IP=$(ip -4 -oneline address show dev $HOST_VETH_NAME scope global | grep --perl-regexp --only-matching "(?<=inet )\Q$IP_PREFIX\E\.[0-9]+/[0-9]+(?= )"); then
+  HOST_IP=${HOST_IP%/*}
+  last_part=${HOST_IP##*.}
+  CTR_IP=$IP_PREFIX.$((last_part+1))
+else
+  # get all ip related the ip range, both host and containers, encode them into a comma started and comma separated string,
+  # e.g. ",10.8.8.1,10.8.8.2,"
+  IP_LIST=,$({ ip -4 -oneline address show scope global | grep --perl-regexp --only-matching "(?<= )\Q$IP_PREFIX\E\.[0-9]+(?=/)" || true; } | tr '\n' ',')
+  # if it has not been assigned an ip with specified range, then assign an unused ip
+  FOUND=0
+  for i in {0..63}; do
+    HOST_IP=$IP_PREFIX.$((i*4+1))
+    CTR_IP=$IP_PREFIX.$((i*4+2)) # for container
+    # found if the ip is not included in the ip list
+    if [[ $IP_LIST != *,$HOST_IP,* && $IP_LIST != *,$CTR_IP,* ]]; then
+      FOUND=1
+      break
+    fi
+  done
+  if [[ $FOUND != 1 ]]; then
+    echo "no free ip in $IP_PREFIX.*/30" >&2
+    exit 1
+  fi
+  # it is very important that each veth pair use different broadcast ip, that is why here use /30
+  ip address add $HOST_IP/30 broadcast + dev $HOST_VETH_NAME
 fi
 
 ################################################################################
 # assign container side veth ip if not yet.
-
 # nsenter is used to enter the container's net namespace
-export IP_PREFIX IP_LIST HOST_IP
-export -f config_ip
-CTR_IP=$(nsenter --target=$CTR_PID --net bash <<'EOF_CTR_BASH'
+nsenter --target=$CTR_PID --net bash <<EOF_CTR_BASH
   set -e -o pipefail -x # exit on error, enable command trace
-
-  # host0 is the nic name of veth in the container, renamed by systemd-nspawn
-  _VETH_NAME=host0
-  config_ip
-  echo $_IP
-
-  # also add a default route via this host veth so that the container can access outside via the host
+  ip address change $CTR_IP/30 broadcast + dev host0
   if ! ip route show default | grep -E "^default via $HOST_IP " >&2; then
-    ip route add default via $HOST_IP dev $_VETH_NAME
+    ip route add default via $HOST_IP dev host0
   fi
 EOF_CTR_BASH
-)
 
 ################################################################################
 # add iptable rule to allow the container access outer network by ip masquerading
